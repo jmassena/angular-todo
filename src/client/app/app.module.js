74450bb3bc43bcc96d2fb8d5cf067a07cf7b3dd4
justin massena <justin@massena.com>
2015-04-27 11:58:55 -0700

    moved directives to own directory

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -16,44 +16,6 @@
       ]);
 
 
-    angular.module('app')
-      .directive('resetFormValidation', resetFormValidation);
 
-    function resetFormValidation(){
-      return{
-        restrict: 'A'
-        ,scope: {
-          vm: '=scope' // 2 way binding
-          ,reset: '=resetFormValidation'
-        }
-
-        ,link: function(scope, element, attrs){
-
-          scope.$watch('reset', function(newVal, oldVal){
-
-            if(newVal){
-              // resolve form name in controller
-              // i.e. form name is 'vm.myForm'
-              var formName = element[0].name;
-              var dotPath = formName.split('.');
-              var target = scope;
-              while(dotPath.length > 0){
-                if(target.hasOwnProperty(dotPath[0])){
-                  target = target[dotPath[0]];
-                }
-                else{break;}
-                dotPath.shift();
-              }
-
-              if(!target){return;}
-
-              target.$setUntouched();
-              target.$setPristine();
-              scope.reset = false;
-            }
-          });
-        }
-      };
-    }
 
 })(this.angular);
---
+++ src/client/app/directives/form-validation-reset.directive.js
@@ -0,0 +1,52 @@
+/**
+ * Calls form.$setUntouched() and $setPristine() when reset-form-validation attribute is true
+ * Usage:
+      Set the reset-form-validation and scope attributes.
+      These are 2 way bindings so the reset-form-validation can be set to false
+      after the form functions are called.
+          <form
+            name="vm.todoForm"
+            reset-form-validation="vm.resetFormFlag"
+            scope="vm">
+
+ */
+
+ angular.module('app')
+  .directive('resetFormValidation', resetFormValidation);
+
+function resetFormValidation(){
+  return{
+    restrict: 'A'
+    ,scope: {
+      vm: '=scope' // 2 way binding
+      ,reset: '=resetFormValidation'
+    }
+
+    ,link: function(scope, element, attrs){
+
+      scope.$watch('reset', function(newVal, oldVal){
+
+        if(newVal){
+          // resolve form name in controller
+          // i.e. form name is 'vm.myForm'
+          var formName = element[0].name;
+          var dotPath = formName.split('.');
+          var target = scope;
+          while(dotPath.length > 0){
+            if(target.hasOwnProperty(dotPath[0])){
+              target = target[dotPath[0]];
+            }
+            else{break;}
+            dotPath.shift();
+          }
+
+          if(!target){return;}
+
+          target.$setUntouched();
+          target.$setPristine();
+          scope.reset = false;
+        }
+      });
+    }
+  };
+}
